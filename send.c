4f18b23926c223a7b05cd9e27f828ca105e39366
Jim Zhong <zhonghc@staff.zjutv.com>
2017-02-22 20:57:08 +0800

    send

--- send.c
+++ send.c
@@ -6,7 +6,11 @@ int main()
     lora_init();
     lora_config(8, 45, 125, 8, 0x34);
 
-    lora_tx("Hello world", 11);
+    while (1)
+    {
+        lora_tx("Hello world", 11);
+        delay(200);
+    }
     lora_cleanup();
     return 0;
 }
