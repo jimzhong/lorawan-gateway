94153f324f51710e9465b5d4e1ea502c96ef8832
Jim Zhong <zhonghc@staff.zjutv.com>
2017-02-22 20:39:24 +0800

    recv

---
+++ recv.c
@@ -0,0 +1,21 @@
+#include "radio.h"
+#include <stdio.h>
+
+int main()
+{
+    uint8_t rxbuf[128];
+    int len;
+
+    lora_init();
+    lora_config(8, 45, 125, 8, 0x34);
+
+    len = lora_rx_single(rxbuf, 1024);
+    printf("%d\n", len);
+    if (len > 0)
+    {
+        dump_hex(rxbuf, len);
+    }
+
+    lora_cleanup();
+    return 0;
+}
