2bd858dda805c7d74b1592265ff33ef5107a5c71
Jim Zhong <zhonghc@staff.zjutv.com>
2017-02-24 12:56:02 +0800

    rx continuous

--- recv.c
+++ recv.c
@@ -15,18 +15,18 @@ int main()
     rx_info_t data;
     int len;
 
-    signal(SIG_INT, stop);
+    signal(SIGINT, stop);
 
     lora_init();
     lora_config(11, 46, 125);
 
-    lora_rx_continuous_start();
+    lora_rx_continuous_start(1000);
 
     while (!stopping)
     {
         lora_rx_continuous_get(&data);
         fprintf(stderr, "SNR=%d, RSSI=%d, CR=%d, TM=%ld\n", data.snr, data.rssi, data.cr, data.ms);
-        dump_hex(dats.buf, data.len);
+        dump_hex(data.buf, data.len);
     }
 
     lora_rx_continuous_stop();
