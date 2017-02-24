d7154c441fb75d74650d032516bbc0de96785529
Jim Zhong <zhonghc@staff.zjutv.com>
2017-02-24 17:55:38 +0800

    send

--- send.c
+++ send.c
@@ -2,15 +2,27 @@
 #include "wiringPi.h"
 #include <stdio.h>
 
+uint8_t packet[] = {0x5a, 0x0e, 0xfa};
+
+volatile int running = 1;
+
+void stop()
+{
+    running = 0;
+    lora_rx_continuous_stop();
+    lora_cleanup();
+    exit(0);
+}
+
 int main()
 {
     lora_init();
-    lora_config(11, 45, 125);
+    lora_config(11, 46, 125);
     lora_set_txpower(15);
-    while (1)
+    while (running)
     {
-        lora_tx("Hello world", 11);
-        delay(200);
+        lora_tx(packet, 3);
+        delay(1000);
     }
     lora_cleanup();
     return 0;
