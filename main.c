27139b915b5e27fdc41a638ff4d6b58cd965667b
Jim Zhong <zhonghc@staff.zjutv.com>
2017-03-12 18:06:02 +0800

    dbeug

--- main.c
+++ main.c
@@ -90,7 +90,7 @@ void timer_set_expire_at(int fd, long second, long nanosecond)
     new_value.it_interval.tv_sec = 0;
     new_value.it_interval.tv_nsec = 0;
 
-    fprintf(stderr, "Setting fd %d to expire at %ld.%ld\n", fd, second, nanosecond);
+    // fprintf(stderr, "Setting fd %d to expire at %ld.%ld\n", fd, second, nanosecond);
 
     if (timerfd_settime(fd, TFD_TIMER_ABSTIME, &new_value, NULL) == -1)
     {
@@ -128,6 +128,8 @@ void queue_tx_request(tx_request_t *req)
             assert(tx_queue[i] != NULL);
             memmove(tx_queue[i], req, sizeof(tx_request_t));
             // start the corresponding timer
+            fprintf(stderr, "TX Request: SF=%u,CR=%u,BW=%u,TXP=%u\n,FREQ=%lu,SEC=%llu,NS=%lu", \
+                req->sf, req->cr, req->bw, req->txpower, req->txfreq, req->second, req->nanosecond);
             timer_set_expire_at(timerfd[i], req->second, req->nanosecond);
             fprintf(stderr, "Queued a TX request of %d bytes.\n", req->len);
             return;
